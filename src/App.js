b2283d8362aabfab387279c0a7c02c61bc8c37a5
Giri <giridharangm@outlook.com>
2017-12-21 15:44:53 +0300

    cosmetic

--- src/App.js
+++ src/App.js
@@ -1,37 +1,37 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import FormRenderer from "./FormRenderer";
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import FormRenderer from './FormRenderer'
 
 const schema = {
   first_name: {
-    label: "First Name",
+    label: 'First Name',
     position: {
       row: 1,
       column: 3
     },
-    groupName: "Contact",
+    groupName: 'Contact',
     control: props => <input {...props} />
   },
   middle_name: {
-    label: "Middle Name",
+    label: 'Middle Name',
     position: {
       row: 1,
       column: 1
     },
-    groupName: "Contact",
+    groupName: 'Contact',
     control: props => <input {...props} />
   },
   last_name: {
-    label: "Last Name",
+    label: 'Last Name',
     position: {
       row: 1,
       column: 2
     },
-    groupName: "Personal",
+    groupName: 'Personal',
     control: props => <textarea {...props} />
   }
-};
+}
 
 class App extends Component {
   render() {
@@ -39,8 +39,8 @@ class App extends Component {
       <div className="App">
         <FormRenderer schema={schema} />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- src/FormRenderer.js
+++ src/FormRenderer.js
@@ -1,26 +1,26 @@
-import React from "react";
-import { Button, Tab, Form, Grid } from "semantic-ui-react";
+import React from 'react'
+import { Button, Tab, Form, Grid } from 'semantic-ui-react'
 
 const FormRenderer = ({ schema }) => {
-  const fieldLabels = Object.keys(schema);
-  const rows = new Set(fieldLabels.map(field => schema[field].position.row));
+  const fieldLabels = Object.keys(schema)
+  const rows = new Set(fieldLabels.map(field => schema[field].position.row))
   const rowControls = Array.from(rows).map(value => {
     const rowFields = fieldLabels.filter(
       field => schema[field].position.row === value
-    );
+    )
     const columns = new Set(
       rowFields.map(field => schema[field].position.column)
-    );
+    )
     const columnControls = Array.from(columns).map(value => {
       const giri = rowFields.filter(
         field => schema[field].position.column === value
-      );
-    });
-    const controls = rowFields.map(field => schema[field].control);
+      )
+    })
+    const controls = rowFields.map(field => schema[field].control)
     const welcome = rowFields
       .sort((a, b) => schema[a].position.column > schema[b].position.column)
       .map((field, index) => {
-        const Control = schema[field].control;
+        const Control = schema[field].control
         return (
           <Grid.Column key={index}>
             <Form.Field>
@@ -28,9 +28,9 @@ const FormRenderer = ({ schema }) => {
               <Control placeholder={schema[field].label} />
             </Form.Field>
           </Grid.Column>
-        );
-      });
-    const rows = new Set(fieldLabels.map(field => schema[field].position.row));
+        )
+      })
+    const rows = new Set(fieldLabels.map(field => schema[field].position.row))
     return (
       <Grid.Row key={value} columns={welcome.length}>
         {/* {controls.map((Control, index) => (
@@ -43,13 +43,13 @@ const FormRenderer = ({ schema }) => {
         ))} */}
         {welcome}
       </Grid.Row>
-    );
-  });
-  const tabs = new Set(fieldLabels.map(field => schema[field].groupName));
+    )
+  })
+  const tabs = new Set(fieldLabels.map(field => schema[field].groupName))
   const tabPanes = Array.from(tabs).map(tab => {
     const controls = fieldLabels
       .filter(field => schema[field].groupName === tab)
-      .map(field => schema[field].control);
+      .map(field => schema[field].control)
     return {
       menuItem: tab,
       render: () => (
@@ -64,19 +64,21 @@ const FormRenderer = ({ schema }) => {
           </div>
         </Tab.Pane>
       )
-    };
-  });
-  const fields = fieldLabels.reduce((acl, curr) => {}, {});
-  console.log(tabPanes);
+    }
+  })
+  const fields = fieldLabels.reduce((acl, curr) => {}, {})
+  console.log(tabPanes)
   return (
     <div>
-      <Button content="asd" />
+      {/* <Button content="asd" /> */}
+      <h1>With Grouping (Tabbed View)</h1>
       <Tab menu={{ secondary: true, pointing: true }} panes={tabPanes} />
+      <h1>Inline Grid View</h1>
       <Form>
         <Grid>{rowControls}</Grid>
       </Form>
     </div>
-  );
-};
+  )
+}
 
-export default FormRenderer;
+export default FormRenderer
